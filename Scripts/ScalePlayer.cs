c6625eedbdba41f3910d61f48350d178364c84f1
txngiam <txngiam@hotmail.com>
2023-06-25 12:52:54 +0200

    initial script commit

---
+++ Scripts/ScalePlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ScalePlayer : MonoBehaviour
+{
+    GameObject Player = GameObject.FindGameObjectsWithTag("Player");
+
+    [SerializeField]
+    private float Scale = 1.0f;
+
+    private void Resize()
+    {
+        Player.transform.localScale = Vector3.one * Scale;
+    }
+
+    void Start()
+    {
+        Resize();
+    }
+}
